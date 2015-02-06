5c225add394a44c52d60c002e73225553a69ff45
Matthew Kay <matthew.kay@gmail.com>
2015-02-05 17:08:01 -0800

    update sponsors page to point to becoming a sponsor info (closes #14)

--- sponsors/become-a-sponsor.php
+++ sponsors/become-a-sponsor.php
@@ -1,48 +1,73 @@
-<?php 
-	$pageTitle = 'Become a Sponsor';
-	$section = 'sponsors';
-	$basePath = '..';
-	include_once('../_inc/header.php');
+<?php
+$pageTitle = 'Become a Sponsor';
+$section = 'sponsors';
+$basePath = '..';
+include_once ('../_inc/header.php');
 ?>
-	<div class="container main">
-		<div class="row">
-			<article class="ten columns" role="main">
-				<h1>Become a Sponsor</h1>
-				<p>The expense of holding the CSCW conference is paid from two sources of income: registration fees paid by the attendees and sponsorships from corporations, universities, and other institutions. Sponsorships are very important to the financial health of CSCW conferences, and the assistance provided by our sponsors is greatly appreciated. Sponsors’ funds allow CSCW conferences to hold down the cost of registration, especially for students.
-				</p>
-				<p>CSCW <?php echo $year; ?> will provide benefits to sponsors in exchange for their financial support. The principal benefit is public acknowledgment of sponsors in the opening and closing plenary sessions, on the conference website, in the proceedings and final program, and on tote bags. The conference will also acknowledge our more generous sponsors for their contribution to well attended social events including receptions, the conference banquet/party, coffee breaks, and student lunches. Their contribution to these events is noted in the conference program and on signs at the events. 
-				</p>
-				<p>Sponsors are given the opportunity to provide information (e.g., brochures) directly to CSCW attendees. Higher level sponsors are invited to present demonstrations of their relevant technology. CSCW <?php echo $year; ?> will also accommodate sponsors interested in recruiting conference attendees. Sponsors may also receive complimentary CSCW <?php echo $year; ?> registrations.
-				</p>
-				<p>Sponsors contribute to the CSCW conference community in myriad ways, including funding. We recognize the valuable contributions of our sponsors, and some CSCW participants in one year become sponsoring firm researchers the next. We are ready to discuss how you would like to contribute or benefit from the conference. 
-				</p>
-				<p>CSCW <?php echo $year; ?> offers four levels of sponsorship: Champion, Benefactor, Contributor, and Friend. This year, for the first time, all champion-level sponsors will be recognized with signs at the conference banquet.
-					<ul>
-						<li>Champions ($15,000 or greater contribution)</li>
-						<li>Benefactors ($10,000 or greater contribution)</li>
-						<li>Contributors ($5,000 or greater contribution)</li>
-						<li>Friends (contribution of less than $5,000)</li>
-					</ul>
-				</p>
-				<p>Benefits of sponsorship levels can be tailored to meet the needs of sponsors. Please contact the Sponsorship co-chairs at <a href="mailto:sponsorship<?php echo $year; ?>@cscw.acm.org">sponsorship<?php echo $year; ?>@cscw.acm.org</a> about becoming a sponsor for CSCW <?php echo $year; ?>.
-				</p>
+<div class="container main">
+	<div class="row">
+		<aside class="four columns offset-by-one">
+			<div class="item">
+				<h2>Submitting</h2>
+				<ul class="subnav" id="nav_sponsor">
+					<li><a href="index.php">Overview</a></li>
+					<li><a href="become-a-sponsor.php" class="active">Become a Sponsor</a></li>
+				</ul>
+			</div>
+		</aside>
+		<article class="ten columns" role="main">
+			<h1>Become a Sponsor</h1>
+			<p>The expense of holding the CSCW conference is paid from two
+				sources of income: registration fees paid by the attendees and
+				sponsorships from corporations, universities, and other
+				institutions. Sponsorships are very important to the financial
+				health of CSCW conferences, and the assistance provided by our
+				sponsors is greatly appreciated. Sponsors’ funds allow CSCW
+				conferences to hold down the cost of registration, especially for
+				students.</p>
+			<p>CSCW <?php echo $year; ?> will provide benefits to sponsors in 
+				exchange for their financial support. The principal benefit is 
+				public acknowledgment of sponsors in the opening and closing 
+				plenary sessions, on the conference website, in the proceedings 
+				and final program, and on tote bags. The conference will also 
+				acknowledge our more generous sponsors for their contribution to 
+				well attended social events including receptions, the conference 
+				banquet/party, coffee breaks, and student lunches. Their 
+				contribution to these events is noted in the conference program 
+				and on signs at the events.</p>
+			<p>Sponsors are given the opportunity to provide information (e.g., 
+				brochures) directly to CSCW attendees. Higher level sponsors are 
+				invited to present demonstrations of their relevant technology. 
+				CSCW <?php echo $year; ?> will also accommodate sponsors 
+				interested in recruiting conference attendees. Sponsors may also 
+				receive complimentary CSCW <?php echo $year; ?> registrations.</p>
+			<p>Sponsors contribute to the CSCW conference community in myriad
+				ways, including funding. We recognize the valuable contributions of
+				our sponsors, and some CSCW participants in one year become
+				sponsoring firm researchers the next. We are ready to discuss how
+				you would like to contribute or benefit from the conference.</p>
+			<p>CSCW <?php echo $year; ?> offers four levels of sponsorship: 
+				Champion, Benefactor, Contributor, and Friend. All champion-level 
+				sponsors will be recognized with signs at the conference banquet.</p>
+			<ul>
+				<li>Champions ($15,000 or greater contribution)</li>
+				<li>Benefactors ($10,000 or greater contribution)</li>
+				<li>Contributors ($5,000 or greater contribution)</li>
+				<li>Friends (contribution of less than $5,000)</li>
+			</ul>
+			<p>Benefits of sponsorship levels can be tailored to meet the needs of
+				sponsors. Please contact the Sponsorship co-chairs at <a
+					href="mailto:cscw<?php echo $year; ?>sponsorship@acm.org">cscw<?php echo $year; ?>sponsorship@acm.org</a>
+				about becoming a sponsor for CSCW <?php echo $year; ?>.</p>
 			<h2 class="h3">Sponsorship Co-Chairs</h2>
-			<p>Sean Goggins, University of Missouri<br />
-				Steve Poltrock<br />
-				<a href="mailto:sponsorship<?php echo $year; ?>@cscw.acm.org">sponsorship<?php echo $year; ?>@cscw.acm.org</a>
-				</p>
-			</article>
-			<aside class="five columns offset-by-one omega">
-				<div class="item">
-					<h2>Submitting</h2>
-					<ul class="subnav" id="nav_sponsor">
-						<li><a href="index.php">Overview</a></li>
-						<li><a href="become-a-sponsor.php" class="active">Become a Sponsor</a></li>
-					</ul>
-				</div>
-			</aside>
-		</div>	
+			<p>
+				Wendy Ju, Stanford University<br /> 
+				Steve Poltrock, Boeing (retired)<br /> 
+				<a href="mailto:cscw<?php echo $year; ?>sponsorship@acm.org">cscw<?php echo $year; ?>sponsorship@acm.org</a>
+			</p>
+		</article>
 	</div>
-<?php 
-	include_once('../_inc/footer.php');
+</div>
+<?php
+include_once ('../_inc/footer.php');
 ?>
