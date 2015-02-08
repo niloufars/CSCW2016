8a92114d7bf70de3bc088fd1d39123890e1994b7
Matthew Kay <matthew.kay@gmail.com>
2015-02-07 19:56:51 -0800

    add program stub pointing people to CFPs. resolves #15

--- program/index.php
+++ program/index.php
@@ -1,35 +1,39 @@
-<?php 
-	$pageTitle = "CSCW $year Program";
-	$section = 'program';
-	$basePath = '..';
-	include_once('../_inc/header.php');
+<?php
+$pageTitle = "CSCW $year Program";
+$section = 'program';
+$basePath = '..';
+include_once ('../_inc/header.php');
 ?>
-	<div class="container main">
-		<div class="row">
-			<article class="ten columns" role="main">
-				<h1>Program | Schedule Overview</h1>
-				<p>Please check back often as updates become available!</p>
-				<p>&nbsp;</p>
-				<p>&nbsp;</p>
-				<p>&nbsp;</p>
-            </article>
-            <aside class="five columns offset-by-one omega">
-                <div class="item">
-                    <h2>Submitting</h2>
-                    <ul class="subnav" id="nav_program">
-                        <li><a href="index.php" class="active">Overview</a></li>
-                        <!--<li><a href="keynotes.php">Keynote Speakers</a></li>-->
-                        <!--<li><a href="papers.php">Papers</a></li>-->
-                        <li><a href="accepted_workshops.php">Workshops</a></li>
-                        <!--<li><a href="posters.php">Interactive Posters</a></li>-->
-                        <!--<li><a href="panels.php">Panels</a></li>-->
-                        <!--<li><a href="dc.php">Doctoral Colloquium</a></li>-->
-                        <!--<li><a href="demos.php">Demonstrations</a></li>-->
-                    </ul>
-                </div>
-            </aside>
-        </div>	
-    </div>
-<?php 
-	include_once('../_inc/footer.php');
+<div class="container main">
+	<div class="row">
+		<article class="ten columns" role="main">
+			<h1>Program</h1>
+			<p>
+				Check back later as updates become available. See the <a
+					href="<?php echo $basePath; ?>/submit/">calls for participation</a>
+				for information on how to submit to CSCW 2016.
+			</p>
+			<p>&nbsp;</p>
+			<p>&nbsp;</p>
+			<p>&nbsp;</p>
+		</article>
+		<aside class="five columns offset-by-one omega">
+			<div class="item">
+				<h2>Program</h2>
+				<ul class="subnav" id="nav_program">
+					<li><a href="index.php" class="active">Overview</a></li>
+					<!--<li><a href="keynotes.php">Keynote Speakers</a></li>-->
+					<!--<li><a href="papers.php">Papers</a></li>-->
+					<!--<li><a href="accepted_workshops.php">Workshops</a></li> -->
+					<!--<li><a href="posters.php">Interactive Posters</a></li>-->
+					<!--<li><a href="panels.php">Panels</a></li>-->
+					<!--<li><a href="dc.php">Doctoral Colloquium</a></li>-->
+					<!--<li><a href="demos.php">Demonstrations</a></li>-->
+				</ul>
+			</div>
+		</aside>
+	</div>
+</div>
+<?php
+include_once ('../_inc/footer.php');
 ?>
