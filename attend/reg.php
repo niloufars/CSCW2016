5012744fb3c930d9d9b48c6cdf6a0f7b3e228465
Matthew Kay <matthew.kay@gmail.com>
2015-07-05 21:52:06 -0700

    formatting cleanup

--- attend/reg.php
+++ attend/reg.php
@@ -1,34 +1,33 @@
-<?php 
-	$pageTitle = 'Attending CSCW';
-	$section = 'attend';
-	$basePath = '..';
-	include_once('../_inc/header.php');
+<?php
+$pageTitle = 'Attending CSCW';
+$section = 'attend';
+$basePath = '..';
+include_once ('../_inc/header.php');
 ?>
-	<div class="container main">
-		<div class="row">
+<div class="container main">
+	<div class="row">
 		<aside class="four columns offset-by-one">
-				<div class="item">
-					<h2>Attending</h2>
-					<ul class="subnav" id="nav_attend">
-						<li><a href="index.php">Overview</a></li>
-						<li><a href="reg.php"  class="active">Conference Registration</a></li>
-						<li><a href="hotel.php">Hotel</a></li>
-						<li><a href="visas.php">Visas</a></li>
-						<li><a href="travel.php">Travel &amp; Transportation</a></li>
-						<li><a href="local.php">Local Attractions</a></li>
-					</ul>
-				</div>
-			</aside>
-			<article class="ten columns" role="main">
-								<h1>Registration</h1>
-				<p>Please check back for rates, deadlines, and further information about registering for CSCW <?php echo $year; ?>.</p>
-				<p>&nbsp;</p>
-				<p>&nbsp;</p>
-				<p>&nbsp;</p>
-			</article>
-			
-		</div>	
+			<div class="item">
+				<h2>Attending</h2>
+				<ul class="subnav" id="nav_attend">
+					<li><a href="index.php">Overview</a></li>
+					<li><a href="reg.php" class="active">Conference Registration</a></li>
+					<li><a href="hotel.php">Hotel</a></li>
+					<li><a href="visas.php">Visas</a></li>
+					<li><a href="travel.php">Travel &amp; Transportation</a></li>
+					<li><a href="local.php">Local Attractions</a></li>
+				</ul>
+			</div>
+		</aside>
+		<article class="ten columns" role="main">
+			<h1>Registration</h1>
+			<p>Please check back for rates, deadlines, and further information about registering for CSCW <?php echo $year; ?>.</p>
+			<p>&nbsp;</p>
+			<p>&nbsp;</p>
+			<p>&nbsp;</p>
+		</article>
 	</div>
-<?php 
-	include_once('../_inc/footer.php');
+</div>
+<?php
+include_once ('../_inc/footer.php');
 ?>
