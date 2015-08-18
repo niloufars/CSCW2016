dece7d8b63a5094f234dcbf00b8f98bd9c58b60c
Matthew Kay <matthew.kay@gmail.com>
2015-08-18 00:26:09 -0700

    add preliminary program; closes #50

--- program/index.php
+++ program/index.php
@@ -6,28 +6,510 @@ include_once ('../_inc/header.php');
 ?>
 <div class="container main">
 	<div class="row">
-<!-- 		<aside class="four columns offset-by-one"> -->
-<!-- 			<div class="item"> -->
-<!-- 				<h2>Program</h2> -->
-<!-- 				<ul class="subnav" id="nav_program"> -->
-<!-- 					<li><a href="index.php" class="active">Overview</a></li> -->
-<!-- 					<li><a href="keynotes.php">Keynote Speakers</a></li> -->
-<!-- 					<li><a href="papers.php">Papers</a></li> -->
-<!-- 					<li><a href="accepted_workshops.php">Workshops</a></li> -->
-<!-- 					<li><a href="posters.php">Interactive Posters</a></li> -->
-<!-- 					<li><a href="panels.php">Panels</a></li> -->
-<!-- 					<li><a href="dc.php">Doctoral Colloquium</a></li> -->
-<!-- 					<li><a href="demos.php">Demonstrations</a></li> -->
-<!-- 				</ul> -->
-<!-- 			</div> -->
-<!-- 		</aside> -->
+		<!-- 		<aside class="four columns offset-by-one"> -->
+		<!-- 			<div class="item"> -->
+		<!-- 				<h2>Program</h2> -->
+		<!-- 				<ul class="subnav" id="nav_program"> -->
+		<!-- 					<li><a href="index.php" class="active">Overview</a></li> -->
+		<!-- 					<li><a href="keynotes.php">Keynote Speakers</a></li> -->
+		<!-- 					<li><a href="papers.php">Papers</a></li> -->
+		<!-- 					<li><a href="accepted_workshops.php">Workshops</a></li> -->
+		<!-- 					<li><a href="posters.php">Interactive Posters</a></li> -->
+		<!-- 					<li><a href="panels.php">Panels</a></li> -->
+		<!-- 					<li><a href="dc.php">Doctoral Colloquium</a></li> -->
+		<!-- 					<li><a href="demos.php">Demonstrations</a></li> -->
+		<!-- 				</ul> -->
+		<!-- 			</div> -->
+		<!-- 		</aside> -->
 		<article class="ten columns offset-by-five" role="main">
-			<h1>Program</h1>
-			<p>
-				Check back later as updates become available. See the <a
-					href="<?php echo $basePath; ?>/submit/">calls for participation</a>
-				for information on how to submit to CSCW 2016.
-			</p>
+			<h1>Preliminary Program</h1>
+			<p>This preliminary program contains information about the timing of
+				CSCW sessions, and the titles (themes) of the paper sessions. 142
+				papers have been accepted for this year’s core program (individual
+				paper titles will be added to this online program in late October,
+				once camera-ready papers have been submitted). Details on workshops,
+				panels, and plenary sessions will be available later this fall, as
+				well.</p>
+			<h2>Saturday</h2>
+			<div class="responsive-table">
+				<table>
+					<thead>
+						<tr>
+							<th>Start Time</th>
+							<th>End Time</th>
+							<th colspan="2">Session</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>9:00</td>
+							<td>5:00</td>
+							<td colspan="2">Workshops</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+			<h2>Sunday</h2>
+			<div class="responsive-table">
+				<table>
+					<thead>
+						<tr>
+							<th>Start Time</th>
+							<th>End Time</th>
+							<th colspan="2">Session</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>9:00</td>
+							<td>5:00</td>
+							<td colspan="2">Workshops & Doctoral Symposium</td>
+						</tr>
+						<tr>
+							<td>5:00</td>
+							<td>6:30</td>
+							<td colspan="2">Welcome Reception (Hyatt Regency)</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+			<h2>Monday</h2>
+			<div class="responsive-table">
+				<table>
+					<thead>
+						<tr>
+							<th>Start Time</th>
+							<th>End Time</th>
+							<th colspan="2">Session</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>9:00</td>
+							<td>10:30</td>
+							<td colspan="2">Opening & Keynote</td>
+						</tr>
+						<tr>
+							<td>10:30</td>
+							<td>11:00</td>
+							<td colspan="2">Coffee Break</td>
+						</tr>
+						<tr>
+							<td>11:00</td>
+							<td>12:30</td>
+							<td colspan="2">Session #1</td>
+						</tr>
+						<tr>
+							<td></td>
+							<td></td>
+							<td>Track #1</td>
+							<td>Papers: Crowd Workers</td>
+						</tr>
+						<tr>
+							<td></td>
+							<td></td>
+							<td>Track #2</td>
+							<td>Papers: Modeling Social Media</td>
+						</tr>
+						<tr>
+							<td></td>
+							<td></td>
+							<td>Track #3</td>
+							<td>Papers: Museums and Public Spaces</td>
+						</tr>
+						<tr>
+							<td></td>
+							<td></td>
+							<td>Track #4</td>
+							<td>Papers: Towards Physical and Social Wellness</td>
+						</tr>
+						<tr>
+							<td></td>
+							<td></td>
+							<td>Track #5</td>
+							<td>Panel (Topic TBA)</td>
+						</tr>
+						<tr>
+							<td>12:30</td>
+							<td>2:30</td>
+							<td colspan="2">Lunch</td>
+						</tr>
+						<tr>
+							<td>2:30</td>
+							<td>4:00</td>
+							<td colspan="2">Session #2</td>
+						</tr>
+						<tr>
+							<td></td>
+							<td></td>
+							<td>Track #1</td>
+							<td>Papers: Distributed Teams</td>
+						</tr>
+						<tr>
+							<td></td>
+							<td></td>
+							<td>Track #2</td>
+							<td>Papers: Digital Learning Environments</td>
+						</tr>
+						<tr>
+							<td></td>
+							<td></td>
+							<td>Track #3</td>
+							<td>Papers: Distance, Coordination, and Motivation</td>
+						</tr>
+						<tr>
+							<td></td>
+							<td></td>
+							<td>Track #4</td>
+							<td>Papers: Relationships and Romance</td>
+						</tr>
+						<tr>
+							<td></td>
+							<td></td>
+							<td>Track #5</td>
+							<td>Panel (Topic TBA)</td>
+						</tr>
+						<tr>
+							<td>4:00</td>
+							<td>4:30</td>
+							<td colspan="2">Coffee Break</td>
+						</tr>
+						<tr>
+							<td>4:30</td>
+							<td>6:00</td>
+							<td colspan="2">Session #3</td>
+						</tr>
+						<tr>
+							<td></td>
+							<td></td>
+							<td>Track #1</td>
+							<td>Papers: Co-present and Remote Media Use</td>
+						</tr>
+						<tr>
+							<td></td>
+							<td></td>
+							<td>Track #2</td>
+							<td>Papers: Credibility and Social Media</td>
+						</tr>
+						<tr>
+							<td></td>
+							<td></td>
+							<td>Track #3</td>
+							<td>Papers: Managing Personal Data</td>
+						</tr>
+						<tr>
+							<td></td>
+							<td></td>
+							<td>Track #4</td>
+							<td>Papers: International Insights</td>
+						</tr>
+						<tr>
+							<td></td>
+							<td></td>
+							<td>Track #5</td>
+							<td>Papers: Rich Telepresence</td>
+						</tr>
+						<tr>
+							<td>6:00</td>
+							<td>8:00</td>
+							<td colspan="2">Poster & Demo Reception</td>
+						</tr>
+						<tr>
+							<td>8:00</td>
+							<td>10:30</td>
+							<td colspan="2">Sponsored Hospitality Receptions</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+			<h2>Tuesday</h2>
+			<div class="responsive-table">
+				<table>
+					<thead>
+						<tr>
+							<th>Start Time</th>
+							<th>End Time</th>
+							<th colspan="2">Session</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>9:00</td>
+							<td>10:30</td>
+							<td colspan="2">Session #4</td>
+						</tr>
+						<tr>
+							<td></td>
+							<td></td>
+							<td>Track #1</td>
+							<td>Papers: Newcomers in Peer Production</td>
+						</tr>
+						<tr>
+							<td></td>
+							<td></td>
+							<td>Track #2</td>
+							<td>Papers: Games</td>
+						</tr>
+						<tr>
+							<td></td>
+							<td></td>
+							<td>Track #3</td>
+							<td>Papers: Online Communities</td>
+						</tr>
+						<tr>
+							<td></td>
+							<td></td>
+							<td>Track #4</td>
+							<td>Papers: Healthcare Providers</td>
+						</tr>
+						<tr>
+							<td></td>
+							<td></td>
+							<td>Track #5</td>
+							<td>Panel (Topic TBA)</td>
+						</tr>
+						<tr>
+							<td>10:30</td>
+							<td>11:00</td>
+							<td colspan="2">Coffee Break</td>
+						</tr>
+						<tr>
+							<td>11:00</td>
+							<td>12:30</td>
+							<td colspan="2">Session #5</td>
+						</tr>
+						<tr>
+							<td></td>
+							<td></td>
+							<td>Track #1</td>
+							<td>Papers: Engaging the Crowd</td>
+						</tr>
+						<tr>
+							<td></td>
+							<td></td>
+							<td>Track #2</td>
+							<td>Papers: Education in the Classroom and Beyond</td>
+						</tr>
+						<tr>
+							<td></td>
+							<td></td>
+							<td>Track #3</td>
+							<td>Papers: Ethics and Policy</td>
+						</tr>
+						<tr>
+							<td></td>
+							<td></td>
+							<td>Track #4</td>
+							<td>Papers: Hospital Work</td>
+						</tr>
+						<tr>
+							<td></td>
+							<td></td>
+							<td>Track #5</td>
+							<td>Panel (Topic TBA)</td>
+						</tr>
+						<tr>
+							<td>12:30</td>
+							<td>2:30</td>
+							<td colspan="2">Lunch</td>
+						</tr>
+						<tr>
+							<td>2:30</td>
+							<td>4:00</td>
+							<td colspan="2">Session #6</td>
+						</tr>
+						<tr>
+							<td></td>
+							<td></td>
+							<td>Track #1</td>
+							<td>Papers: Crowd-Powered Applications</td>
+						</tr>
+						<tr>
+							<td></td>
+							<td></td>
+							<td>Track #2</td>
+							<td>Papers: Social Network Methods</td>
+						</tr>
+						<tr>
+							<td></td>
+							<td></td>
+							<td>Track #3</td>
+							<td>Papers: Hacking, Making, and Discovering</td>
+						</tr>
+						<tr>
+							<td></td>
+							<td></td>
+							<td>Track #4</td>
+							<td>Papers: Food and Health</td>
+						</tr>
+						<tr>
+							<td></td>
+							<td></td>
+							<td>Track #5</td>
+							<td>Panel (Topic TBA)</td>
+						</tr>
+						<tr>
+							<td>4:00</td>
+							<td>4:30</td>
+							<td colspan="2">Coffee Break</td>
+						</tr>
+						<tr>
+							<td>4:30</td>
+							<td>5:30</td>
+							<td colspan="2">Lasting Impact Plenary Session</td>
+						</tr>
+						<tr>
+							<td>5:30</td>
+							<td>6:00</td>
+							<td colspan="2">Town Hall Meeting</td>
+						</tr>
+						<tr>
+							<td>7:00</td>
+							<td>10:30</td>
+							<td colspan="2">Conference Banquet (Exploratorium)</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+			<h2>Wednesday</h2>
+			<div class="responsive-table">
+				<table>
+					<thead>
+						<tr>
+							<th>Start Time</th>
+							<th>End Time</th>
+							<th colspan="2">Session</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>9:00</td>
+							<td>10:30</td>
+							<td colspan="2">Session #7</td>
+						</tr>
+						<tr>
+							<td></td>
+							<td></td>
+							<td>Track #1</td>
+							<td>Papers: Crowd Innovation and Crowdfunding</td>
+						</tr>
+						<tr>
+							<td></td>
+							<td></td>
+							<td>Track #2</td>
+							<td>Papers: Non-Profits and Humanitarian Responses</td>
+						</tr>
+						<tr>
+							<td></td>
+							<td></td>
+							<td>Track #3</td>
+							<td>Papers: Work and Work Environments</td>
+						</tr>
+						<tr>
+							<td></td>
+							<td></td>
+							<td>Track #4</td>
+							<td>Papers: Parents and Children</td>
+						</tr>
+						<tr>
+							<td></td>
+							<td></td>
+							<td>Track #5</td>
+							<td>Panel (Topic TBA)</td>
+						</tr>
+						<tr>
+							<td>10:30</td>
+							<td>11:00</td>
+							<td colspan="2">Coffee Break</td>
+						</tr>
+						<tr>
+							<td>11:00</td>
+							<td>12:30</td>
+							<td colspan="2">Session #8</td>
+						</tr>
+						<tr>
+							<td></td>
+							<td></td>
+							<td>Track #1</td>
+							<td>Papers: Multimedia Creation and Remixing</td>
+						</tr>
+						<tr>
+							<td></td>
+							<td></td>
+							<td>Track #2</td>
+							<td>Papers: Unpacking Social Networks</td>
+						</tr>
+						<tr>
+							<td></td>
+							<td></td>
+							<td>Track #3</td>
+							<td>Papers: Open Science and Infrastructure</td>
+						</tr>
+						<tr>
+							<td></td>
+							<td></td>
+							<td>Track #4</td>
+							<td>Papers: Accessibility and Universal Design</td>
+						</tr>
+						<tr>
+							<td></td>
+							<td></td>
+							<td>Track #5</td>
+							<td>Panel (Topic TBA)</td>
+						</tr>
+						<tr>
+							<td>12:30</td>
+							<td>2:30</td>
+							<td colspan="2">Lunch</td>
+						</tr>
+						<tr>
+							<td>2:30</td>
+							<td>4:00</td>
+							<td colspan="2">Session #9</td>
+						</tr>
+						<tr>
+							<td></td>
+							<td></td>
+							<td>Track #1</td>
+							<td>Papers: Crowd Workflows</td>
+						</tr>
+						<tr>
+							<td></td>
+							<td></td>
+							<td>Track #2</td>
+							<td>Papers: Mobile Design and Usage</td>
+						</tr>
+						<tr>
+							<td></td>
+							<td></td>
+							<td>Track #3</td>
+							<td>Papers: Computer-Mediated Communication</td>
+						</tr>
+						<tr>
+							<td></td>
+							<td></td>
+							<td>Track #4</td>
+							<td>Papers: Home and Family</td>
+						</tr>
+						<tr>
+							<td></td>
+							<td></td>
+							<td>Track #5</td>
+							<td>Panel (Topic TBA)</td>
+						</tr>
+						<tr>
+							<td>4:00</td>
+							<td>4:30</td>
+							<td colspan="2">Coffee Break</td>
+						</tr>
+						<tr>
+							<td>4:30</td>
+							<td>6:00</td>
+							<td colspan="2">Closing Plenary</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
 		</article>
 	</div>
 </div>
