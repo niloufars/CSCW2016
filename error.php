75447714b847cd85ee786834de6380aa31091822
Matthew Kay <matthew.kay@gmail.com>
2015-08-18 00:29:49 -0700

    tone down shouting in 404 message

--- error.php
+++ error.php
@@ -7,11 +7,8 @@
 	<div class="container main">
 		<div class="row">
 			<article class="ten columns offset-by-five" role="main">
-				<h1>404 Page not found
-				<br />
-				<br />
-				If this problem persists, please contact <a href="mailto:cscw<?php echo $year; ?>webmasters@acm.org">cscw<?php echo $year; ?>webmasters@acm.org</a>.</h1>
-				<br />
+				<h1>404 Page not found</h1>
+				<p>If this problem persists, please contact <a href="mailto:cscw<?php echo $year; ?>webmasters@acm.org">cscw<?php echo $year; ?>webmasters@acm.org</a>.</p>
 				<h2>Graphic Design &amp; Webmastering</h2>
 				<p>Matthew Kay, University of Washington<br />
 				Niloufar Salehi, Stanford University<br />
