f6aaa23e0a5da040b2083643271235c0659264cc
Matthew Kay <matthew.kay@gmail.com>
2015-09-18 21:35:34 -0700

    update attractions and transportation, closes #53

--- attend/local.php
+++ attend/local.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 $pageTitle = 'Attending CSCW';
 $section = 'attend';
 $basePath = '..';
@@ -19,13 +19,207 @@ include_once('../_inc/header.php');
 				</ul>
 			</div>
 		</aside>
-		<article class="ten columns" role="main">		
-			<h1>Attractions</h1>
-			<p>San Fransisco is host to many popular tourist attractions. Please come back later for more information.
-			</p>			
+		<article class="ten columns" role="main">
+			<h1>Local Attractions</h1>
+
+			<h2>Tourist Attractions</h2>
+			<ul>
+				<li>Ferry Building - Right across the street from the conference
+					hotel, San Francisco&rsquo;s ferry building is a ferry terminal, a
+					shopping/food destination, and on the weekend an epic outdoor
+					farmers and crafts market. A great place to grab lunch between
+					sessions! (<a href="http://www.ferrybuildingmarketplace.com/">http://www.ferrybuildingmarketplace.com/</a>)
+				</li>
+				<li>Fisherman&rsquo;s Wharf - A classic San Francisco attraction, go
+					for the fun seaside attractions, stay for the sea lions. (<a
+					href="http://visitfishermanswharf.com/">http://visitfishermanswharf.com/</a>)
+				</li>
+				<li>Golden Gate Bridge - Iconic Golden Gate Bridge connects San
+					Francisco to Marin County. It&rsquo;s scenic to visit from the SF
+					side, and you can walk or bike onto or across it, even going so far
+					as Sausalito for lunch. Bikes are available for rent all over San
+					Francisco. (<a href="http://goldengatebridge.org/visitors/">http://goldengatebridge.org/visitors/</a>)
+				</li>
+				<li>Coit Tower - Located in Telegraph Hill, walking distance from
+					the conference hotel, the 1922 Art Deco tower and its art make an
+					easy and beautiful outing from the conference. (<a
+					href="http://sfrecpark.org/destination/telegraph-hill-pioneer-park/coit-tower/">http://sfrecpark.org/destination/telegraph-hill-pioneer-park/coit-tower/</a>)
+				</li>
+				<li>Alcatraz - You need to take a ferry to Alcatraz, which is a bit
+					pricy, but in our opinion quite worthwhile if you have the time and
+					interest; it is a visually and emotionally captivating excursion.
+					Ferries are available at Pier 33. (<a
+					href="http://www.nps.gov/alca/index.htm">http://www.nps.gov/alca/index.htm</a>)
+				</li>
+				<li>Cable cars - Hang off the side of one of San Francisco&rsquo;s
+					iconic cable cars. You can catch them from a variety of stations
+					along Market Street in downtown San Francisco, or ride along the
+					Embarcadero (where the hotel is located) in a vintage car. (<a
+					href="http://www.sfcablecar.com/">http://www.sfcablecar.com/</a>)
+				</li>
+				<li>Chinatown - The largest Chinatown outside of Asia and the oldest
+					in North America is best explored on foot. Eat some delicious
+					Chinese food and explore the many stores (<a
+					href="http://www.sanfranciscochinatown.com">http://www.sanfranciscochinatown.com</a>)
+				</li>
+				<li>Ghirardelli Square - Originally a chocolate factory, this square
+					now hosts a variety of waterfront stores and restaurants and, of
+					course, some delicious chocolate and ice cream! (<a
+					href="http://www.ghirardellisq.com">http://www.ghirardellisq.com</a>).
+				</li>
+				<li>Japanese Tea Garden - Don&rsquo;t miss the oldest public
+					Japanese Garden in the U.S., which is also where the fortune cookie
+					was introduced to Americans. Enjoy the serene atmosphere while you
+					sip some tea in their tea house (<a
+					href="http://japaneseteagardensf.com">http://japaneseteagardensf.com</a>)
+				</li>
+			</ul>
+
+			<h2>Restaurants</h2>
+			<ul>
+				<li>Aziza/Mourad - Chef Mourad Lahlou&rsquo;s restaurants serve
+					creative Moroccan food in traditional ambience. Mourad is
+					newly-opened and more pricey but with beautiful surroundings (<a
+					href="http://mouradsf.com">http://mouradsf.com</a>)
+				</li>
+				<li>Burma Superstar - Never had Burmese food? You need to try this.
+					Locations in San Francisco and Oakland. (<a
+					href="http://www.burmasuperstar.com/">http://www.burmasuperstar.com/</a>)
+				</li>
+				<li>Chez Panisse - Alice Waters&rsquo; famed Berkeley eatery is
+					worth a trek (the BART train is a good way to get there).
+					Reservations are hard to come by, but plan in advance either for
+					the upstairs casual bistro, or the much fancier (and pricier) pre
+					fixe downstairs dining room. (<a href="http://www.chezpanisse.com/">http://www.chezpanisse.com</a>)
+				</li>
+				<li>Dosa on Valencia - South Indian fare with a California take. Get
+					reservations in advance. Locations on Valencia St and Fillmore St.
+					(<a href="http://dosasf.com">http://dosasf.com</a>)
+				</li>
+				<li>Flour + Water - SFs top pizza and fancy Italian. Tough to get
+					into. Go early and be prepared to wait, but it&rsquo;s worth it. (<a
+					href="http://www.flourandwater.com/">http://www.flourandwater.com/</a>)
+				</li>
+				<li>Nopalito - Traditional Mexican food with local and organic
+					ingredients. (<a href="http://www.nopalitosf.com">http://www.nopalitosf.com</a>)
+				</li>
+				<li>Slanted Door - Chef Charles Phan&rsquo;s nationally-acclaimed
+					Vietnamese restaurant in the Ferry building has great views of the
+					Bay and delicious food. (<a href="http://www.slanteddoor.com">http://www.slanteddoor.com</a>)
+				</li>
+				<li>Yank Sing - Amazing and authentic dim sum in the Rincon Center,
+					close to the conference hotel. Crowded. Worth it. (<a
+					href="http://www.yanksing.com/">http://www.yanksing.com/</a>)
+				</li>
+			</ul>
+			
+			<h2>Nature &amp; Exercise</h2>
+			<ul>
+				<li>Golden Gate Park - Golden Gate Park has tons to do whether
+					you&rsquo;re up for a quick visit or a longer stay. Lots of trails
+					for hiking and biking, botanical gardens, the California Academy of
+					Sciences, and the De Young museum, just to name a few (there is
+					even a herd of bison that live in the park!). To get there grab the
+					N-Judah Muni train from Embarcadero, right across from the
+					conference hotel. (<a
+					href="http://sfrecpark.org/parks-open-spaces/golden-gate-park-guide/">http://sfrecpark.org/parks-open-spaces/golden-gate-park-guide/</a>)
+				</li>
+				<li>East Bay Parks - The hills above Berkeley and Oakland are a
+					continuous string of parks. Hike for miles and witness stunning
+					views of the bay starting in Tilden park which is accessible and
+					wonderful above Berkeley. (<a
+					href="http://www.ebparks.org/parks/tilden.htm">http://www.ebparks.org/parks/tilden.htm</a>)
+				</li>
+				<li>Bay Trail: Enjoy walking or riding along this ~340 mile trail
+					along the Bay. (<a href="http://baytrail.org">http://baytrail.org</a>)
+				</li>
+				<li>The Presidio - If you&rsquo;re seeking nature, head here to
+					enjoy majestic woods and stunning views of the Golden Gate Bridge
+					and the Bay. (<a href="http://www.nps.gov/prsf/index.htm">http://www.nps.gov/prsf/index.htm</a>)
+				</li>
+			</ul>
+			
+			<h2>Museums</h2>
+			<ul>
+				<li>De Young - In Golden Gate Park, museum with a view. (<a
+					href="http://deyoung.famsf.org/">http://deyoung.famsf.org/</a>)
+				</li>
+				<li>SF MOMA - The SF Museum of Modern Art is closed for a major
+					renovation, but they have mobile exhibits at sites all over the Bay
+					Area. (<a href="http://www.sfmoma.org/">http://www.sfmoma.org/</a>)
+				</li>
+				<li>Asian Art Museum - One of the largest collections of Asian art
+					in the world (<a href="http://www.asianart.org">http://www.asianart.org</a>)
+				</li>
+				<li>California Academy of Sciences - Part zoo, part natural history
+					museum, part planetarium, part aquarium, there&rsquo;s an awful lot
+					to see here! Go early if you go on the weekend. (<a
+					href="http://www.calacademy.org/">http://www.calacademy.org/</a>)
+				</li>
+				<li>Museum of the African Diaspora: Explore the history, art, and
+					culture of the African Diaspora (<a href="http://www.moadsf.org">http://www.moadsf.org</a>)
+				</li>
+				<li>Oakland Museum of California: Learn about the science, history
+					and art of the Golden State (<a href="http://museumca.org">http://museumca.org</a>)
+				</li>
+				<li>Lawrence Hall of Science: Offers science exhibits and an
+					interactive planetarium (<a
+					href="http://www.lawrencehallofscience.org">http://www.lawrencehallofscience.org</a>)
+				</li>
+				<li>Chabot Space &amp; Science Center: The planetarium and
+					telescopes are fun for kids and adults alike (<a
+					href="http://www.chabotspace.org/index.htm">http://www.chabotspace.org/index.htm</a>)
+				</li>
+				<li>Tech Museum of Innovation (Peninsula) - Enjoy a variety of tech
+					exhibits from social robots to virtual worlds in downton San Jose (<a
+					href="https://www.thetech.org">https://www.thetech.org</a>)
+				</li>
+				<li>Computer History Museum (Peninsula) - For computer history
+					buffs, this is worth a trip to Mountain View to experience the
+					world&rsquo;s largest collection of computing artifacts (<a
+					href="http://www.computerhistory.org">http://www.computerhistory.org</a>)
+				</li>
+				<li>And, of course, just down the street from the hotel is the
+					famous Exploratorium science museum, where the conference banquet
+					will be held on Tuesday evening!</li>
+			</ul>
+			
+			<h2>Kids &amp; Family</h2>
+			<ul>
+				<li>San Francisco Zoo</li>
+				<li>Oakland Zoo</li>
+				<li>Exploratorium</li>
+			</ul>
+			
+			<h2>Bigger Adventures</h2>
+			<ul>
+				<li>Wine Country - Visit Napa, Sonoma, or Russian River for some
+					scenic country and amazing wine. You&rsquo;ll need to rent a car or
+					catch a tour bus from San Francisco</li>
+				<li>Half Moon Bay - Head South along the peninsula to the quaint
+					beach town where, among other things, you can get great fish tacos.</li>
+				<li>Santa Cruz - Further south (1.5-2 hours from SF) there&rsquo;s
+					tons to do in Santa Cruz, including the iconic beach boardwalk.</li>
+				<li>Muir Wood and Mt. Tam - Muir Woods is perhaps the most famous of
+					the Redwood forests. Think awe inspiring trees and ferny
+					undergrowth. Again you&rsquo;ll need to either catch a tour or have
+					access to a car to get there, but it&rsquo;s typically an hour or
+					less to get there. Bring a good jacket.</li>
+				<li>Sausalito - Just over the Golden Gate Bridge, Sausalito is
+					lovely on a sunny day, and a great place for window shopping and a
+					bite in a lovely Bay setting.</li>
+				<li>Monterey and Carmel-by-the-Sea - Even further south than Santa
+					Cruz are Monterey and Carmel. Perhaps the most accessible
+					attraction is Monterey Bay Aquarium, which is 100% worth a visit if
+					you make it down there.</li>
+				<li>Mount Diablo (East Bay): This 3,849 ft mountain provides
+					spectacular views of the Bay area. You can drive to the summit or
+					if you have time for an adventure, there are hiking trails to the
+					top!</li>
+			</ul>
 		</article>
 
-	</div>	
+	</div>
 </div>
 <?php 
 include_once('../_inc/footer.php');
--- attend/travel.php
+++ attend/travel.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 $pageTitle = 'Attending CSCW';
 $section = 'attend';
 $basePath = '..';
@@ -20,28 +20,71 @@ include_once('../_inc/header.php');
 			</div>
 		</aside>
 		<article class="ten columns" role="main">
-			<h1>Travel &amp; Transportation Information</h1>
+			<h1>Travel &amp; Transport</h1>
+
+			<h2>Air Travel</h2>
 			<p>
-				The major gateway to San Francisco is <a href="http://www.flysfo.com/" class="text" target="_blank">San Francisco International airport (SFO)</a>, 15
-				miles south of the city. Transportation from the airport to downtown San Francisco
-				is available via BART (Bay Area Rapid Transport). To get to the conference hotel, get
-				off at the Embarcadero station – a 2-min walk from the conference venue. 
+				Three major airports service the greater San Francisco region. The
+				major gateway to San Francisco is <a href="http://www.flysfo.com/">San
+					Francisco International airport (SFO)</a>, 15 miles south of the
+				city. Transportation from the airport to downtown San Francisco is
+				available via <a href="https://www.bart.gov/">BART</a> - San
+				Francisco&rsquo;s light rail service. The BART station at SFO is
+				located inside the International Terminal. To get to the conference
+				hotel, take BART to the Embarcadero station &ndash; a 30 minute ride
+				that will set you back about $9. The conference venue is directly
+				next to the Embarcadero station.
+			</p>
+
+			<p>Oakland International Airport (OAK), located across the Bay, is
+				also connected to the venue via BART. The recently completed AirBART
+				service provides a connection from just outside Oakland&rsquo;s main
+				terminal to the Coliseum BART Station. The journey will take about
+				40 minutes and cost about $10.</p>
+
+			<p>The conference venue is also accessible from San Jose
+				International Airport (SJC). Traveling from SJC to the venue is
+				significantly more difficult and time consuming, however. To get to
+				the conference venue from SJC, take the VTA bus 10/Airport Flyer
+				toward &ldquo;Santa Clara Caltrain&rdquo;; Take Caltrain North to
+				the Millbrae stop; Transfer to BART at Millbrae and take it North to
+				the Embarcadero stop.</p>
+
+			<h2>Getting Around SF</h2>
+			<p>
+				BART - BART is the Bay Area&rsquo;s light rail system, and
+				it&rsquo;s among the most reliable and cost effective ways to get
+				around. <a href="http://www.bart.gov/">bart.gov</a> provides plenty
+				of details to help you plan your trip. Several great apps are also
+				available to help you plan and keep track of trains in real time.
+			</p>
+			<p>Note that BART does not run 24 hours. The last trains generally
+				leave the end of the line for their final run at midnight, and
+				don&rsquo;t start up again until 4am.</p>
+			<p>
+				Muni - Muni is San Francisco&rsquo;s bus and trolley system. While
+				BART only runs one major artery through the city, Muni will take you
+				all over, from Fisherman&rsquo;s Wharf to Ocean Beach. <a
+					href="https://www.sfmta.com">https://www.sfmta.com</a> is your
+				source to plan your trip on Muni.
 			</p>
 			<p>
-				Oakland International airport, located across the Bay, is also connected to the venue
-				via BART or a 35-min drive from the conference venue. 
+				AC Transit - If you want to travel around the East Bay by bus, or
+				get back and forth from Berkeley/Oakland to SF, you&rsquo;ll need AC
+				Transit. AC Transit does run bus service back and forth from the
+				East Bay all night, so you&rsquo;ll never be stuck if you&rsquo;re
+				out late. All things AC Transit at: <a
+					href="http://www.actransit.org">http://www.actransit.org</a>
 			</p>
-			<p>
-				The furthest away is San Jose International Airport (SJC). Using public
-				transportation, take the VTA bus 10/Airport Flyer toward “Santa Clara Caltrain”;
-				Take Caltrain North to the Millbrae stop; Transfer to BART at Millbrae and take it
-				North to the Embarcadero stop. 
-			</p>
-			
+			<p>Taxis/Uber/Lyft - Traditional taxis are common in San Francisco,
+				but Uber and Lyft are ubiquitous. You can generally get a Lyft or
+				Uber to take you anywhere in the city in just a few minutes. Make
+				sure to load up the apps before you arrive.</p>
+
 		</article>
-	
-	</div>	
+
+	</div>
 </div>
-<?php 
-include_once('../_inc/footer.php');
+<?php
+include_once ('../_inc/footer.php');
 ?>
